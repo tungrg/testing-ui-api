a2e6860c760cfc4fe9a9ae63493662391e249cc9
tungrg <18120258@student.hcmus.edu.vn>
2021-11-01 14:41:37 +0700

    -add api test

--- src/test/java/com/tungrg/testing/RestAssure44/RestAssure4Test.java
+++ src/test/java/com/tungrg/testing/RestAssure44/RestAssure4Test.java
@@ -131,4 +131,68 @@ public class RestAssure4Test extends  RestAssureBaseTest{
 
         rp.then().assertThat().log().all().body("location.tz_id",equalTo("Asia/Ho_Chi_Minh"));
     }
+    @Test
+    public void testLat(){
+        String baseURL = "https://api.weatherapi.com/v1";
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec
+                .setBaseUri(baseURL)
+                .addParam("key","7eb763e2aa804cef9f931743212210")
+                .addParam("q","Ho_Chi_Minh")
+                .addParam("aqi","yes")
+                .addParam("days","1");
+
+        Response rp = RestAssured.given(spec.build())
+                .get("/forecast.json");
+
+        rp.then().assertThat().log().all().body("location.lat",equalTo(new Float(10.75)));
+    }
+    @Test
+    public void testLong(){
+        String baseURL = "https://api.weatherapi.com/v1";
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec
+                .setBaseUri(baseURL)
+                .addParam("key","7eb763e2aa804cef9f931743212210")
+                .addParam("q","Ho_Chi_Minh")
+                .addParam("aqi","yes")
+                .addParam("days","1");
+
+        Response rp = RestAssured.given(spec.build())
+                .get("/forecast.json");
+
+        rp.then().assertThat().log().all().body("location.lon",equalTo(new Float(106.67)));
+    }
+    @Test
+    public void testCurrentTempC(){
+        String baseURL = "https://api.weatherapi.com/v1";
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec
+                .setBaseUri(baseURL)
+                .addParam("key","7eb763e2aa804cef9f931743212210")
+                .addParam("q","Ho_Chi_Minh")
+                .addParam("aqi","yes")
+                .addParam("days","1");
+
+        Response rp = RestAssured.given(spec.build())
+                .get("/forecast.json");
+
+        rp.then().assertThat().log().all().body("current.temp_c",equalTo(new Float(34)));
+    }
+    @Test
+    public void testCurrentTempF(){
+        String baseURL = "https://api.weatherapi.com/v1";
+        RequestSpecBuilder spec = new RequestSpecBuilder();
+        spec
+                .setBaseUri(baseURL)
+                .addParam("key","7eb763e2aa804cef9f931743212210")
+                .addParam("q","Ho_Chi_Minh")
+                .addParam("aqi","yes")
+                .addParam("days","1");
+
+        Response rp = RestAssured.given(spec.build())
+                .get("/forecast.json");
+
+        rp.then().assertThat().log().all().body("current.temp_f",equalTo(new Float(93.2)));
+    }
 }
